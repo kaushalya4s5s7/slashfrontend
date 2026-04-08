e2a86b2b613ef022c3ae75ba187f7f63800fbc12
kaushalya4s5s7 <chaudharikaushal02@gmail.com>
2026-04-08 11:13:34 +0530

    feat(frontend): update app/split/context/split-context.ts

---
+++ app/split/context/split-context.ts
@@ -0,0 +1,9 @@
+export type SplitContextInfo = {
+  feature: "split";
+  summary: "Approve slashXTZ and mint PT/YT using splitter";
+};
+
+export const splitContext: SplitContextInfo = {
+  feature: "split",
+  summary: "Approve slashXTZ and mint PT/YT using splitter",
+};
